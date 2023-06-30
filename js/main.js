8786762b462abbd497123b2fcf07b6305ae54bfe
BeauTheBeau <beauthebeau@protonmail.me>
2023-06-30 21:26:23 +0100

    Update CSS properties, remove unused HTML elements and amend script configuration
    Updated CSS properties such as overflow-x and max-width for better alignment, and amended background color gradient for visual enhancement in both index.scss and index.css files. Removed unused swiper HTML elements, specifically pagination, navigation buttons and scrollbar in index.html for cleaner code. Amended .swiper configuration in main.js, disabling loop and adjusting effect, freeMode and autoplay parameters for an improved user experience.

--- js/main.js
+++ js/main.js
@@ -3,21 +3,14 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.brow
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+  loop: false,
   spaceBetween: 30,
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  effect: 'cards',
+  freeMode: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
