e5918ca3d447293ea9978b7eb8a465f147d7cbee
BeauTheBeau <beauthebeau@protonmail.me>
2023-07-15 15:29:56 +0100

    Remove redundant JavaScript and CSS files
    Deleted js/vendor/modernizr-3.11.2.min.js, js/plugins.js, mbt+.html, and css/index.css.map. The modernizr and plugins JavaScripts were removed as they were unused and were causing unnecessary bloat in the codebase. Similarly, the CSS map file was not needed for the current state of the application. Furthermore, the HTML file mbt+.html was also removed as it was not connected with the rest of the application.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -8,87 +8,69 @@ const swiper = new Swiper('.swiper', {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
-
   }
 });
 
-// If hash is #subscribe
-if (window.location.hash === "#subscribe" && document.getElementById("subscribe")) document.getElementById("payment").showModal();
-
-
 /**
- * Floating Navbar
+ * @name: Floating Navbar
  * @type {Element}
- * description: When the user scrolls down, the navbar will detach from the top and float.
- *              When the user scrolls up, the navbar will reattach to the top and take up 100% of the width.
- *              This is done by adding and removing the "floating" class, defined in css/index.scss
+ * @description: When the user scrolls down, the navbar will detach from the top and float.
+ *               When the user scrolls up, the navbar will reattach to the top and take up 100% of the width.
+ *               We do this by adding and removing the "floating" class, defined in css/index.scss
  */
 
-const navbar = document.getElementsByClassName("floating__links")[0]
-let scrollY = window.scrollY;
+// const navbar = document.getElementsByClassName("navbar")[0]
+// let scrollY = window.scrollY;
+//
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > scrollY) navbar.classList.add("floating")
+//    else navbar.classList.remove("floating")
+//   scrollY = window.scrollY;
+// })
+
+/**
+ * @name: Slowly increase opacity of .hero's "before" pseudo-element as the user scrolls down
+ * @type {Element}
+ * description: We do this by reading the scroll position and setting the opacity of .hero::before
+ *             to the percentage of the scroll position divided by the height of the hero section,
+ *             then setting the opacity of .hero::before (getComputedStyle) to that value.
+ */
+
+
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > scrollY) navbar.classList.add("floating")
-   else navbar.classList.remove("floating")
-  scrollY = window.scrollY;
-})
+  const heroHeight = document.getElementsByClassName("hero")[0].offsetHeight;
+  const opacity = (Math.min(window.scrollY / heroHeight, 1) * 2).toFixed(2);
+  const blur = 20 - (Math.min(window.scrollY / heroHeight, 1) * 10).toFixed(2);
+  console.log(opacity, blur);
 
-/**
- * Typewriter Effect
- * @type {Element}
- * @function typeText
- * @returns {void}
- description: Initializes a typewriter effect that displays a series of phrases in a specified HTML element.
- The typewriter effect simulates the process of typing and erasing text, creating a dynamic and engaging presentation.
- */
+  const heroForeground = document.getElementById("hero__foreground");
+  heroForeground.style.opacity = opacity;
+  heroForeground.style.filter = `blur(${blur}px)`;
+  heroForeground.style.y = `${window.scrollY / 2}px`;
+});
 
-const typedSpan = document.getElementById("typed")
-const toType = [
-  "Revolutionizing tank production with self-replicating technology.",
-  "Dominating the Battlefield: Unleash the Power of the Leopard Evo and Revolutionize Armored Warfare.",
-  "Unleash Your Inner Warrior: Experience Unmatched Performance with the Leopard Evo.",
-  "Outmaneuver. Strike. Conquer: Discover the Swift Agility of the Leopard Evo.",
-  "Unleash Devastating Force: The Leopard Evo, Your Key to Battlefield Supremacy.",
-  "Unparalleled Adaptability: The Leopard Evo, the Ultimate Tank for Every Combat Scenario.",
-  "Confidence in Every Battle: Secure Your Investment with Our Exclusive Extended Warranty.",
-  "Unmatched Peace of Mind: Protect Your Tank with Our Limited-Time Extended Warranty.",
-  "Dominate Now and Worry Less: Enjoy 5 Years of Repairs and Replacements with Our Extended Warranty.",
-  "Upgrade Your Armor: Experience the Evolution of Tank Technology with the Leopard Evo.",
-  "The Future of Armored Warfare: Unleash the Self-Replicating Power of the Leopard Evo.",
-]
 
-const delayTyping_char = 50;
-const delayErasing_text = 20;
-const delayTyping_text = 2000;
 
-let toTypeIndex = 0;
-let charIndex = 0;
+import('./typewriter.js').then(({default: typewriter}) => {
+  typewriter(
+    [
+      "Revolutionize tank production with self-replicating technology.",
+      "Leopard Evo: Dominating the Battlefield, Redefining Armored Warfare.",
+      "Unleash Your Inner Warrior: Experience Evo's Unmatched Performance.",
+      "Swift Agility, Unmatched Power: Conquer with Evo.",
+      "Unleash Devastating Force: Your Key to Battlefield Supremacy.",
+      "Unparalleled Adaptability: The Ultimate Tank for Every Combat Scenario.",
+      "Confidence in Every Battle: Secure Your Investment with Exclusive Extended Warranty.",
+      "Unmatched Peace of Mind: Protect Your Tank with Our Limited-Time Extended Warranty.",
+      "Dominate Now, Worry Less: 5 Years of Repairs and Replacements with Extended Warranty.",
+      "Upgrade Your Armor: Experience the Revolution of Tank Technology.",
+      "The Future of Armored Warfare: Unleash the Power of Self-Replication."],
+    document.getElementById("typewriter"),
+    50
+  );
 
-function typeText() {
-  if (charIndex < toType[toTypeIndex].length) {
-    typedSpan.textContent += toType[toTypeIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(typeText, delayTyping_char);
-  }
-  else setTimeout(eraseText, delayTyping_text);
+});
 
-}
 
-function eraseText() {
-  if (charIndex > 0) {
-    typedSpan.textContent = toType[toTypeIndex].substring(0, charIndex-1);
-    charIndex = charIndex-1;
-    setTimeout(eraseText, delayErasing_text);
-  }
-  else {
-    toTypeIndex++;
 
-    if (toTypeIndex >= toType.length) toTypeIndex = 0;
-    setTimeout(typeText, delayTyping_text);
-  }
-}
-
-window.onload = function() {
-  if (!typedSpan) return;
-  if (toType[toTypeIndex].length) setTimeout(typeText, delayTyping_text);
-}
